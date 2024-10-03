3c3d0283e7cfa835229814930fedef2aeda42773
mmmjunjoy <sjbssk0809@gmail.com>
2024-10-03 19:05:56 +0900

    CLOUDTYPE - CONNECT

--- bin/balarm/alarm/consumers.py
+++ bin/balarm/alarm/consumers.py
@@ -3,10 +3,10 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class AlarmConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        # self.room_name ="alarm"
-        # self.room_group_name = "alarm_notifications"
-        user = self.scope['user']
-        self.room_group_name = f"user_{user.id}_notifications"
+        self.room_name ="alarm"
+        self.room_group_name = "alarm_notifications"
+        # user = self.scope['user']
+        # self.room_group_name = f"user_{user.id}_notifications"
 
         await self.channel_layer.group_add(
             self.room_group_name,
