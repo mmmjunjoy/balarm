9f15c1958fb613f15a5c7a659516ee61689ff760
mmmjunjoy <sjbssk0809@gmail.com>
2024-10-24 18:44:40 +0900

    CLOUDTYPE - TASK & UTILS

--- bin/balarm/alarm/utils.py
+++ bin/balarm/alarm/utils.py
@@ -9,6 +9,12 @@ def schedule_alarm(alarm):
     delay = (alarm.date - datetime.now()).total_seconds()
     
 
+     # 알림시간이 현재 시간보다, 전 시간 알림은 알림이 오지 않도록 변경
+    if alarm.date < datetime.now():
+        print(f"이미 지난 알림입니다: {alarm.title} at {alarm.date}")
+        return
+
+
     transaction.on_commit(lambda:(
         print(f"트랜잭션 커밋 후 celery 작업 실행 :{alarm.id} at {datetime.now()}"),
         current_app.send_task(
